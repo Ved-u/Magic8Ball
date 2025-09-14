2045cc64ef05ea0a5e6cb25d3a1ad0fe47572d85
Ved-u <vedantutpat084@gmail.com>
2025-09-14 19:26:45 +0530

    first commit

---
+++ Magic 8 Ball/Magic 8 Ball/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnMagic = New System.Windows.Forms.Button
+        Me.txtQuestion = New System.Windows.Forms.TextBox
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.btnExit = New System.Windows.Forms.Button
+        Me.btnClear = New System.Windows.Forms.Button
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnMagic
+        '
+        Me.btnMagic.Location = New System.Drawing.Point(12, 337)
+        Me.btnMagic.Name = "btnMagic"
+        Me.btnMagic.Size = New System.Drawing.Size(168, 47)
+        Me.btnMagic.TabIndex = 0
+        Me.btnMagic.Text = "Click for Magic"
+        Me.btnMagic.UseVisualStyleBackColor = True
+        '
+        'txtQuestion
+        '
+        Me.txtQuestion.Location = New System.Drawing.Point(127, 311)
+        Me.txtQuestion.Name = "txtQuestion"
+        Me.txtQuestion.Size = New System.Drawing.Size(242, 20)
+        Me.txtQuestion.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(51, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(290, 281)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 314)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Type  Your Question"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(291, 339)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(78, 42)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit Magic 8"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(197, 339)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 40)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear Magic"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(381, 391)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtQuestion)
+        Me.Controls.Add(Me.btnMagic)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Magic 8 Ball"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnMagic As System.Windows.Forms.Button
+    Friend WithEvents txtQuestion As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+
+End Class
---
+++ Magic 8 Ball/Magic 8 Ball/Form1.vb
@@ -0,0 +1,121 @@
+﻿Public Class Form1
+
+    Dim responce(20) As String
+    Public ReadOnly Property appPath() As String
+        Get
+            Dim s As String = Application.StartupPath
+            If s.Substring(s.Length - 1) <> "\" Then s &= "\"
+            Return s
+        End Get
+    End Property
+    Private Sub btnMagic_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMagic.Click
+        If txtQuestion.Text = "" Then
+            Beep()
+            Exit Sub
+        End If
+        If Len(txtQuestion.Text) < 4 Then
+            Exit Sub
+        End If
+        Dim Speech
+        Speech = CreateObject("sapi.spvoice")
+
+        responce(0) = "As I see it, yes"
+        responce(1) = "It is certain"
+        responce(2) = "It is decidedly so"
+        responce(3) = "Most likely"
+        responce(4) = "Outlook good"
+        responce(5) = "Signs point to yes"
+        responce(6) = "Without a doubt"
+        responce(7) = "Yes"
+        responce(8) = "Yes - definitely"
+        responce(9) = "You may rely on it"
+        responce(10) = "Reply hazy, try again"
+        responce(11) = "Ask again later"
+        responce(12) = "Better not tell you now"
+        responce(13) = "Cannot predict now"
+        responce(14) = "Concentrate and ask again"
+        responce(15) = "Don't count on it"
+        responce(16) = "My reply is no"
+        responce(17) = "My sources say no"
+        responce(18) = "Outlook not so good"
+        responce(19) = "Very doubtful"
+        Dim generator As New Random
+        Dim randomValue As Integer
+        randomValue = generator.Next(1, 20)
+
+        If randomValue = 1 Then
+            PictureBox1.BackgroundImage = My.Resources.ball1
+        End If
+        If randomValue = 2 Then
+            PictureBox1.BackgroundImage = My.Resources.ball2
+        End If
+        If randomValue = 3 Then
+            PictureBox1.BackgroundImage = My.Resources.ball3
+        End If
+        If randomValue = 4 Then
+            PictureBox1.BackgroundImage = My.Resources.ball4
+        End If
+        If randomValue = 5 Then
+            PictureBox1.BackgroundImage = My.Resources.ball5
+        End If
+        If randomValue = 6 Then
+            PictureBox1.BackgroundImage = My.Resources.ball6
+        End If
+        If randomValue = 7 Then
+            PictureBox1.BackgroundImage = My.Resources.ball7
+        End If
+        If randomValue = 8 Then
+            PictureBox1.BackgroundImage = My.Resources.ball8
+        End If
+        If randomValue = 9 Then
+            PictureBox1.BackgroundImage = My.Resources.ball9
+        End If
+        If randomValue = 10 Then
+            PictureBox1.BackgroundImage = My.Resources.ball10
+        End If
+        If randomValue = 11 Then
+            PictureBox1.BackgroundImage = My.Resources.ball11
+        End If
+        If randomValue = 12 Then
+            PictureBox1.BackgroundImage = My.Resources.ball12
+        End If
+        If randomValue = 13 Then
+            PictureBox1.BackgroundImage = My.Resources.ball13
+        End If
+        If randomValue = 14 Then
+            PictureBox1.BackgroundImage = My.Resources.ball14
+        End If
+        If randomValue = 15 Then
+            PictureBox1.BackgroundImage = My.Resources.ball15
+        End If
+        If randomValue = 16 Then
+            PictureBox1.BackgroundImage = My.Resources.ball16
+        End If
+        If randomValue = 17 Then
+            PictureBox1.BackgroundImage = My.Resources.ball17
+        End If
+        If randomValue = 18 Then
+            PictureBox1.BackgroundImage = My.Resources.ball18
+        End If
+        If randomValue = 19 Then
+            PictureBox1.BackgroundImage = My.Resources.ball19
+        End If
+        If randomValue = 20 Then
+            PictureBox1.BackgroundImage = My.Resources.ball20
+        End If
+        Speech.Speak(responce(randomValue - 1))
+    End Sub
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        Dim responce As String
+        responce = MessageBox.Show("Are you sure..?", "Confirm choice", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2)
+        If responce = vbYes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        PictureBox1.BackgroundImage = My.Resources._8_Ball_picture_3
+        txtQuestion.Clear()
+    End Sub
+End Class
